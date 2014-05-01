83ac1947ce1f6d3250d0ce865231c426f7edbb48
Tomsik68 <tomsik68@gmail.com>
2014-05-01 10:27:54 +0200

    Changed reference data, working on mapper script

--- mappers/openpandora/mapperScript.sh
+++ mappers/openpandora/mapperScript.sh
@@ -1,4 +1,4 @@
 # This script is part of WifiMapper project
-echo "This is mapper script of WifiMapper project."
+echo "This is mapper script of WifiMapper project. Please read manual before using it."
 
 date +%T
