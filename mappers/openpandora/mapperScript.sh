491ff7d2a16f8101ac1424b5ecabbb6593e9d57d
Tomsik68 <tomsik68@gmail.com>
2014-05-04 17:28:23 +0200

    mapperScript finally works! The tool now works!

--- mappers/openpandora/mapperScript.sh
+++ mappers/openpandora/mapperScript.sh
@@ -9,6 +9,7 @@ echo "This is mapper script of WifiMapper project. Please read manual before usi
 echo "Moving captured files..."
 
 CAPDIR="capture-$RANDOM"
+mkdir $CAPDIR
 mv route $CAPDIR
 mv time $CAPDIR
 mv spots-in-time.json $CAPDIR
@@ -29,6 +30,6 @@ while [ 0 -lt 1 ]; do
   python dump-signal.py $WIFI_INTERFACE $MEASUREMENT
   GPS_DUMP=`gpspipe -w -n 3 | tail --lines 1`
   echo "${MEASUREMENT}	${GPS_DUMP}" >> $ROUTE_FILE
-  ((MEASUREMENT++))
+  MEASUREMENT=$((MEASUREMENT+1))
   sleep $INTERVAL
 done
